d037a75bc6dfd75b8def5ea062c9b4b6149b39ac
김성종 <sjkim@gitcosoft.com>
2023-12-07 17:51:38 +0900

    20231207

--- src/main/reactfront/src/layout/ModalStyles.js
+++ src/main/reactfront/src/layout/ModalStyles.js
@@ -1,9 +1,10 @@
 const modalStyles = {
     content: {
-        width: '50%', // Set the desired width
-        height: '50%', // Set the desired height
+        width: '60%', // Set the desired width
+        height: '60%', // Set the desired height
         margin: 'auto', // Center the modal
         overflow: 'auto',
+        padding: '3%'
     },
     overlay: {
         backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the last value (0.5) for the desired opacity
